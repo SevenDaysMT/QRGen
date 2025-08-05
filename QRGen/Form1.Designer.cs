faa09791a762660755edde041cec63541b48dca6
Seven Days <kaplyaandrey1994@gmail.com>
2025-08-05 11:06:58 +0300

    Добавьте файлы проекта.

---
+++ QRGen/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace QRGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxQRCode = new System.Windows.Forms.PictureBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxQRCode
+            // 
+            this.pictureBoxQRCode.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBoxQRCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxQRCode.Location = new System.Drawing.Point(388, 12);
+            this.pictureBoxQRCode.Name = "pictureBoxQRCode";
+            this.pictureBoxQRCode.Size = new System.Drawing.Size(400, 400);
+            this.pictureBoxQRCode.TabIndex = 0;
+            this.pictureBoxQRCode.TabStop = false;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 46);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(370, 152);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите текст или URL:";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 383);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(370, 29);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Сохранить на Рабочий стол";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(12, 204);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(270, 29);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Сгенерировать";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(288, 204);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 29);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 425);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.pictureBoxQRCode);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QRGen";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxQRCode;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ QRGen/Form1.cs
@@ -0,0 +1,68 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace QRGen
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap qrCodeImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+            pictureBoxQRCode.Image = null;
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (qrCodeImage == null)
+            {
+                MessageBox.Show("Сначала сгенерируйте QR-код.", "QRGen");
+                return;
+            }
+
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string filePath = Path.Combine(desktopPath, "QRCode.png");
+                qrCodeImage.Save(filePath);
+                MessageBox.Show($"QR-код сохранен на рабочий стол: {filePath}", "QRGen");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении QR-кода: {ex.Message}", "QRGen");
+            }
+        }
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string inputText = txtInput.Text;
+
+            if (string.IsNullOrWhiteSpace(inputText))
+            {
+                MessageBox.Show("Пожалуйста, введите текст для генерации QR-кода.", "QRGen");
+                return;
+            }
+
+            try
+            {
+                using QRCodeGenerator qrGenerator = new();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(inputText, QRCodeGenerator.ECCLevel.Q);
+                using QRCode qrCode = new(qrCodeData);
+                qrCodeImage = qrCode.GetGraphic(20);
+                Bitmap resizedImage = new(qrCodeImage, new Size(400, 400));
+                pictureBoxQRCode.Image = resizedImage;
+                pictureBoxQRCode.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при генерации QR-кода: {ex.Message}", "QRGen");
+            }
+        }
+    }
+    
+}
